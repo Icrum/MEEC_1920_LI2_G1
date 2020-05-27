1764acd0eba3a357de602dbf16fc90ca7fb8337b
17985 <a19875@alunos.ipca.pt>
2020-05-27 12:29:20 +0100

    First Commit Luis

---
+++ Previsao/previsao_som.py
@@ -0,0 +1,110 @@
+import pickle
+import librosa
+import pyaudio
+import numpy as np
+import sounddevice as sd
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+
+CHUNK = 2048
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 2
+seconds = 1
+
+scaler = StandardScaler()
+scaler_filename = "E:\\GoogleDrive\\MestradoEEC\\LabInt_2\\MEEC_1920_LI2_G1\\Treino\\sc_model_person.bin"
+scaler = pickle.load(open(scaler_filename,"rb"))
+
+scaler_cmd = StandardScaler()
+scaler_cmd_filename = "E:\\GoogleDrive\\MestradoEEC\\LabInt_2\\MEEC_1920_LI2_G1\\Treino\\sc_model_command.bin"
+scaler_cmd = pickle.load(open(scaler_cmd_filename,"rb"))
+
+mlp = MLPClassifier()
+filename = "E:\\GoogleDrive\\MestradoEEC\\LabInt_2\\MEEC_1920_LI2_G1\\Treino\\trained_model_person.bin"
+mlp = pickle.load(open(filename,"rb"))
+
+mlp_cmd = MLPClassifier()
+filename_cmd = "E:\\GoogleDrive\\MestradoEEC\\LabInt_2\\MEEC_1920_LI2_G1\\Treino\\trained_model_person.bin"
+mlp_cmd = pickle.load(open(filename_cmd,"rb"))
+
+grupo = ("G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8")
+command = ("parar", "recuar", "direita", "esquerda", "baixo", "centro", "cima", "avançar")
+
+def get_audio ():
+
+    signal, sample_rate = librosa.load("E:\GoogleDrive\MestradoEEC\LabInt_2\MEEC_1920_LI2_G1\AudioDatasetRec\G3\G3_Baixo_1.wav", sr=RATE)
+    # p = pyaudio.PyAudio()
+    #
+    # stream = p.open(format=FORMAT,
+    #                 channels=CHANNELS,
+    #                 rate=RATE,
+    #                 input=True,
+    #                 frames_per_buffer=CHUNK)
+    #
+    # print("* recording")
+    #
+    # frames = []
+    #
+    # for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    #     data = stream.read(CHUNK)
+    #     frames.append(data)
+    #
+    # print("* done recording")
+
+    # stream.stop_stream()
+    # stream.close()
+    # p.terminate()
+
+    # print("A detetar!")
+    # frames = sd.rec(int(seconds * RATE), samplerate=RATE, channels=2)
+    # sd.wait()
+    # frames = librosa.to_mono(frames)
+    # print("A processar!")
+
+    return signal
+
+
+def prep_data (clip_audio):
+    mfcc = np.mean(librosa.feature.mfcc(clip_audio, sr=RATE, n_mfcc=13, n_fft=2048, hop_length=512).T,axis=0)
+    X = []
+    X.append(mfcc)
+    X_person = scaler.transform(X)
+
+    return X_person
+
+def prev_result (X_person, X_cmd):
+    predictions = mlp.predict(X_person)
+    predictions_cmd = mlp_cmd.predict(X_cmd)
+
+    result = grupo[int(predictions)]
+    result_cmd = command[int(predictions_cmd)]
+    return result, result_cmd
+
+
+def prep_data_cmd(signal):
+    W = []
+    result = np.array([])
+
+    mfcc = np.mean(librosa.feature.mfcc(signal, sr=RATE, n_mfcc=13, n_fft=2048, hop_length=512).T, axis=0)
+    result = np.hstack((result, mfcc))
+    stft = np.abs(librosa.stft(signal))
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=RATE).T, axis=0)
+    result = np.hstack((result, chroma))
+    mel = np.mean(librosa.feature.melspectrogram(signal, sr=RATE).T, axis=0)
+    result = np.hstack((result, mel))
+    W.append(result)
+
+    X_cmd = scaler_cmd.transform(W)
+    return X_cmd
+
+
+if __name__ == "__main__":
+    clip_audio = get_audio()
+    X_person = prep_data(clip_audio)
+    X_cmd = prep_data_cmd(clip_audio)
+
+    result, result_cmd = prev_result(X_person, X_cmd)
+    print(result)
+    print(result_cmd)
---
+++ Treino/trained_model_person.py
@@ -0,0 +1,67 @@
+import numpy as np
+import os
+import librosa
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.externals.joblib import dump, load
+
+DATASET_PATH = "E:\GoogleDrive\MestradoEEC\LabInt_2\MEEC_1920_LI2_G1\AudioDatasetRec"
+SAMPLE_RATE = 44100
+command = []
+
+def save_mfcc(dataset_path, num_mfcc=13, n_fft=2048, hop_length=512):
+    X=[]
+    y=[]
+
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+        # process all audio files in genre sub-dir
+        semantic_label = dirpath.split("\\")[-1]
+        command.append(semantic_label)
+        for f in filenames:
+            file_path = os.path.join(dirpath, f)
+            signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+            mfcc = np.mean(librosa.feature.mfcc(signal, sr=sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length).T, axis=0)
+            X.append(mfcc)
+            y.append(i - 1)
+            print("{}".format(file_path))
+
+    return X,y
+
+
+if __name__ == "__main__":
+
+    X,y = save_mfcc(DATASET_PATH)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    StandardScaler(copy=True, with_mean=True, with_std=True)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    sc_filename = 'sc_model_person.bin'
+    pickle.dump(scaler, open(sc_filename, 'wb'))
+
+    mlp = MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
+    mlp.fit(X_train, y_train)
+    MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
+                  beta_2=0.999, early_stopping=False, epsilon=1e-08,
+                  hidden_layer_sizes=(13, 13, 13), learning_rate='constant',
+                  learning_rate_init=0.001, max_iter=500, momentum=0.9,
+                  nesterovs_momentum=True, power_t=0.5, random_state=None,
+                  shuffle=True, solver='adam', tol=0.0001, validation_fraction=0.1,
+                  verbose=False, warm_start=False)
+
+    predictions = mlp.predict(X_test)
+
+    filename = 'trained_model_person.bin'
+    pickle.dump(mlp, open(filename, 'wb'))
+
+    print(confusion_matrix(y_test, predictions))
+    print(classification_report(y_test, predictions))
+    print(command)
+
+
