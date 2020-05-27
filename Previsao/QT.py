93ddfed9abbb29759c925f0982d37fdb3ab0ce59
17985 <a19875@alunos.ipca.pt>
2020-05-27 16:19:41 +0100

    Update 1.2 (audio QtLabels)

--- Previsao/QT.py
+++ Previsao/QT.py
@@ -8,20 +8,32 @@ class MyWindow(QMainWindow):
         self.initUI()
 
     def button_clicked(self):
-        self.label.setText("you pressed the button")
-        self.update()
+        self.user.setText("G???")
+        self.comando.setText("Comando ???")
+        #self.update()
 
     def initUI(self):
-        self.setGeometry(200, 200, 800, 600)
-        self.setWindowTitle("Tech With Tim")
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("LI Grupo 1")
 
         self.label = QtWidgets.QLabel(self)
-        self.label.setText("my first label!")
-        self.label.move(50,50)
+        self.label.setText("Utilizador: ")
+        self.label.move(150,550)
+        self.user = QtWidgets.QLabel(self)
+        self.user.setText(" ")
+        self.user.move(250, 550)
+
+        self.label_2 = QtWidgets.QLabel(self)
+        self.label_2.setText("Comando: ")
+        self.label_2.move(400, 550)
+        self.comando = QtWidgets.QLabel(self)
+        self.comando.setText(" ")
+        self.comando.move(500, 550)
 
         self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("click me!")
+        self.b1.setText("Iniciar")
         self.b1.clicked.connect(self.button_clicked)
+        self.b1.move(10, 550)
 
     def update(self):
         self.label.adjustSize()
--- Previsao/previsao_som.py
+++ Previsao/previsao_som.py
@@ -2,9 +2,12 @@ import pickle
 import librosa
 import pyaudio
 import numpy as np
+import sys
 import sounddevice as sd
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
+from PyQt5 import  QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 CHUNK = 2048
 FORMAT = pyaudio.paInt16
@@ -31,6 +34,54 @@ mlp_cmd = pickle.load(open(filename_cmd,"rb"))
 
 grupo = ("G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8")
 command = ("parar", "recuar", "direita", "esquerda", "baixo", "centro", "cima", "avançar")
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow,self).__init__()
+        self.initUI()
+
+    def button_clicked(self):
+        clip_audio = get_audio()
+        X_person = prep_data(clip_audio)
+        X_cmd = prep_data_cmd(clip_audio)
+        result, result_cmd = prev_result(X_person, X_cmd)
+
+        self.user.setText(result)
+        self.comando.setText(result_cmd)
+        print(result)
+        print(result_cmd)
+
+    def initUI(self):
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("LI Grupo 1")
+
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Utilizador: ")
+        self.label.move(150,550)
+        self.user = QtWidgets.QLabel(self)
+        self.user.setText(" ")
+        self.user.move(250, 550)
+
+        self.label_2 = QtWidgets.QLabel(self)
+        self.label_2.setText("Comando: ")
+        self.label_2.move(400, 550)
+        self.comando = QtWidgets.QLabel(self)
+        self.comando.setText(" ")
+        self.comando.move(500, 550)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Iniciar")
+        self.b1.clicked.connect(self.button_clicked)
+        self.b1.move(10, 550)
+
+    def update(self):
+        self.label.adjustSize()
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
 
 def get_audio ():
 
@@ -101,10 +152,12 @@ def prep_data_cmd(clip_audio):
 
 
 if __name__ == "__main__":
-    clip_audio = get_audio()
-    X_person = prep_data(clip_audio)
-    X_cmd = prep_data_cmd(clip_audio)
+    window()
+    # clip_audio = get_audio()
+    # X_person = prep_data(clip_audio)
+    # X_cmd = prep_data_cmd(clip_audio)
+    #
+    # result, result_cmd = prev_result(X_person, X_cmd)
+    # print(result)
+    # print(result_cmd)
 
-    result, result_cmd = prev_result(X_person, X_cmd)
-    print(result)
-    print(result_cmd)
